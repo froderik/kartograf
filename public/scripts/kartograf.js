317c060c3763af6def4869ce48d2e07f58b571f8
Fredrik Rubensson <fredrik@eldfluga.se>
2016-01-01 20:41:05 +0100

    get map setup from markup

--- public/scripts/kartograf.js
+++ public/scripts/kartograf.js
@@ -18,7 +18,10 @@ var addOneMarker = function(feature, map) {
 
 $(document).ready(
     function() {
-	var map = L.map( 'map' ).setView( [40.728, -74], 13 );
+	var mapLat = $('#map').data( 'lat' );
+	var mapLon = $('#map').data( 'lon' );
+	var mapZoom = $('#map').data( 'zoom' );
+	var map = L.map( 'map' ).setView( [mapLat, mapLon], mapZoom );
 	var osmLayerUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
 	var attribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>';
 	var options = { attribution: attribution, maxZoom: 18 };
