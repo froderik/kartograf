6f0097eb51e062f5577f0afc9a15d939d99cf75b
Fredrik Rubensson <fredrik@eldfluga.se>
2016-02-01 15:50:50 +0100

    got rid of the silly extension and aded a post method (yet to be tested), something for the browser extension to call

--- kartograf.rb
+++ kartograf.rb
@@ -10,12 +10,25 @@ class Kartograf < Sinatra::Base
   end
 
   get '/maps/*' do |name|
-    haml :index, locals: { points_url: "/points/#{name}.json" }
+    haml :index, locals: { points_url: "/points/#{name}" }
   end
 
-  get '/points/*.json' do |name|
-    content = redis.get key_for name
-    json content
+  get '/points/*' do |name|
+    json points_for name
+  end
+
+  post '/points/*' do |name|
+    points_json = points_for name
+    points_hash = JSON.parse points_json
+    new_point_json = request.body.read.to_s
+    new_point_hash = JSON.parse new_point_json
+    # TODO : add validation...
+    points_hash['features'] << new_point_hash
+    redis.set key_for( name ), points_hash
+  end
+
+  def points_for name
+    redis.get key_for name
   end
 
   def key_for name
